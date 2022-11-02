dd5ca7acfc52d2369f07651bfaa2aa5a12368799
thomasnielsen00 <you@example.com>
2022-11-02 14:14:15 +0100

    v2

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json());
 
 // Since API is not compatible with v1, API version is increased to v2
-app.use('/api/v2', taskRouter);
+app.use('/api/v2', taskRouter); // Endre navn etterhvert?
 
 export default app;
--- server/src/recipe-service.ts
+++ server/src/recipe-service.ts
@@ -5,9 +5,14 @@ export type Recipe = {
   recipe_id: number;
   name: string;
   category: string;
-  country: string; 
+  country: string;
 };
 
+export type Ingredient = {
+  ingredient_id : number; 
+  name: string;
+
+}
 
 class RecipeService {
   /**
@@ -15,56 +20,68 @@ class RecipeService {
    */
   get(recipe_id: number) {
     return new Promise<Recipe | undefined>((resolve, reject) => {
-      pool.query('SELECT * FROM Recipe WHERE recipe_id = ?', [recipe_id], (error, results: RowDataPacket[]) => {
-        if (error) return reject(error);
+      pool.query(
+        'SELECT * FROM Recipe WHERE recipe_id = ?',
+        [recipe_id],
+        (error, results: RowDataPacket[]) => {
+          if (error) return reject(error);
 
-        resolve(results[0] as Recipe);
-      });
+          resolve(results[0] as Recipe);
+        }
+      );
     });
   }
 
   /**
-   * Get all tasks.
+   * Get all recipes.
    */
   getAll() {
-    return new Promise<Task[]>((resolve, reject) => {
-      pool.query('SELECT * FROM Tasks', (error, results: RowDataPacket[]) => {
+    return new Promise<Recipe[]>((resolve, reject) => {
+      pool.query('SELECT * FROM Recipe', (error, results: RowDataPacket[]) => {
         if (error) return reject(error);
 
-        resolve(results as Task[]);
+        resolve(results as Recipe[]);
       });
     });
   }
+
+
 
   /**
-   * Create new task having the given title.
+   * Create new recipe.
    *
-   * Resolves the newly created task id.
+   * Resolves the newly created recipe_id.
    */
-  create(title: string) {
-    return new Promise<number>((resolve, reject) => {
-      pool.query('INSERT INTO Tasks SET title=?', [title], (error, results: ResultSetHeader) => {
-        if (error) return reject(error);
+  // create(title: string) {
+  //   return new Promise<number>((resolve, reject) => {
+  //     pool.query('INSERT INTO Recipe SET title=?', [title], (error, results: ResultSetHeader) => {
+  //       if (error) return reject(error);
 
-        resolve(results.insertId);
-      });
-    });
-  }
+  //       resolve(results.insertId);
+  //     });
+  //   });
+  // }
 
   /**
    * Delete task with given id.
    */
-  delete(id: number) {
-    return new Promise<void>((resolve, reject) => {
-      pool.query('DELETE FROM Tasks WHERE id = ?', [id], (error, results: ResultSetHeader) => {
-        if (error) return reject(error);
-        if (results.affectedRows == 0) return reject(new Error('No row deleted'));
+  // delete(id: number) {
+  //   return new Promise<void>((resolve, reject) => {
+  //     pool.query('DELETE FROM Tasks WHERE id = ?', [id], (error, results: ResultSetHeader) => {
+  //       if (error) return reject(error);
+  //       if (results.affectedRows == 0) return reject(new Error('No row deleted'));
 
-        resolve();
-      });
-    });
-  }
+  //       resolve();
+  //     });
+  //   });
+  // }
 }
 
-const taskService = new TaskService();
-export default taskService;
+
+class IngredientService {
+  
+}
+
+
+const recipeService = new RecipeService();
+export default recipeService;
