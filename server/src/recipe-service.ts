1757481344bea3615039ed3009b8d05b1d395617
thomasnielsen00 <you@example.com>
2022-11-02 13:13:43 +0100

    start

--- server/src/recipe-service.ts
+++ server/src/recipe-service.ts
@@ -1,22 +1,27 @@
 import pool from './mysql-pool';
 import type { RowDataPacket, ResultSetHeader } from 'mysql2';
 
-export type Task = {
-  id: number;
-  title: string;
-  done: boolean;
+export type Recipe = {
+  recipe_id: number;
+  name: string;
+  category: string;
+  country: string; 
 };
 
-class TaskService {
+
+
+
+
+class RecipeService {
   /**
-   * Get task with given id.
+   * Get recipe with given recipe_id.
    */
-  get(id: number) {
-    return new Promise<Task | undefined>((resolve, reject) => {
-      pool.query('SELECT * FROM Tasks WHERE id = ?', [id], (error, results: RowDataPacket[]) => {
+  get(recipe_id: number) {
+    return new Promise<Recipe | undefined>((resolve, reject) => {
+      pool.query('SELECT * FROM Recipe WHERE recipe_id = ?', [recipe_id], (error, results: RowDataPacket[]) => {
         if (error) return reject(error);
 
-        resolve(results[0] as Task);
+        resolve(results[0] as Recipe);
       });
     });
   }
