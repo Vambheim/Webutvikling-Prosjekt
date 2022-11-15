5cefd9efc6b060850495382bb4c9e3e20d2f77da
bknielse <bknielse@ntnu.no>
2022-11-15 16:32:23 +0100

    r

--- server/test/recipe-router.test.ts
+++ server/test/recipe-router.test.ts
@@ -474,4 +474,11 @@ describe('Fetch recipeIngredients (GET)', () => {
       done();
     });
   });
+
+  test('Fetch all recipeIngredient (500 internal server error)', (done) => {
+    axios.get('/recipes/1/ingredients/').catch((error) => {
+      expect(error.message).toEqual('Request failed with status code 404');
+      done();
+    });
+  });
 });
